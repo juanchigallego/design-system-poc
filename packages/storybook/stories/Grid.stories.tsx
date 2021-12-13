48451065cea87b01c8de9be0fd663bebaed59906
Juan Manuel Gallego <juanma.gallego@tiendanube.com>
2021-12-13 16:54:44 -0300

    build grid stories

--- packages/storybook/stories/Grid.stories.tsx
+++ packages/storybook/stories/Grid.stories.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React from 'react';
+import useState from 'storybook-addon-state';
 
-import { Grid, Text } from "../../components/src";
+import { Button, Grid, Stack, Text } from "../../components/src";
 
 import { color } from "../../tokens/build/js/tokens";
 
@@ -31,36 +32,68 @@ const DemoContent = ({ label, labelSecondary, labelTertiary }:DemoProps ) => {
       height: "90px",
       padding: "0.5rem"
     }}>
-      <Text size="caption" appearance="primary" block>{label}</Text>
-      {labelSecondary && <Text size="caption" appearance="primary" block>{labelSecondary}</Text>}
-      {labelTertiary && <Text size="caption" appearance="primary" block>{labelTertiary}</Text>}
+      <Text size="caption" appearance="primary" lineHeight="heading" block>{label}</Text>
+      {labelSecondary && <Text size="caption" appearance="primary" lineHeight="heading" block>{labelSecondary}</Text>}
+      {labelTertiary && <Text size="caption" appearance="primary" lineHeight="heading" block>{labelTertiary}</Text>}
     </div>
   )
 }
 
-const repeatingCards: JSX.Element[] = [];
-for (let i = 1; i <= 3; i++) {
-  repeatingCards.push(<Grid.Column><DemoContent key={i} label="Content" /></Grid.Column>);
+export const AutoWidthCols = (args) => {
+  const [cardCount, setCardCount] = useState("cards", 3);
+
+  const repeatingCards: JSX.Element[] = [];
+  for (let i = 1; i <= cardCount; i++) {
+    repeatingCards.push(<Grid.Column key={i}><DemoContent label="Column with auto width" /></Grid.Column>);
+  }
+
+  return(
+    <Grid {...args}> 
+      <Grid.Row spacing={args.spacing}>
+        {repeatingCards}
+      </Grid.Row>
+      <Grid.Row>
+        <Stack>
+          <Button onClick={() => setCardCount(cardCount - 1)}>Remove column</Button>
+          <Button onClick={() => setCardCount(cardCount + 1)}>Add column</Button>
+        </Stack>
+      </Grid.Row>
+    </Grid>
+  )
+};
+
+export const FixedWidthCols = (args) => {
+  const [cardCount, setCardCount] = useState("cards", 1);
+
+  const repeatingCards: JSX.Element[] = [];
+  for (let i = 1; i <= cardCount; i++) {
+    repeatingCards.push(<Grid.Column key={i}><DemoContent label="Column with auto width" /></Grid.Column>);
+  }
+
+  return(
+    <Grid {...args}> 
+      <Grid.Row spacing={args.spacing}>
+        <Grid.Column sm={args.sm} md={args.md} lg={args.lg}>
+          <DemoContent label={`Width sm ${args.sm}`} labelSecondary={`Width md ${args.md}`} labelTertiary={`Width lg ${args.lg}`} />
+        </Grid.Column>
+        {repeatingCards}
+      </Grid.Row>
+      <Grid.Row>
+        <Stack>
+          <Button onClick={() => setCardCount(cardCount - 1)}>Remove column</Button>
+          <Button onClick={() => setCardCount(cardCount + 1)}>Add column</Button>
+        </Stack>
+      </Grid.Row>
+    </Grid>
+  )
+};
+
+AutoWidthCols.args = {
+  fluid: false,
+  spacing: "base"
 }
 
-const Template = (args) =>
-  <Grid {...args}> 
-    <Grid.Row spacing={args.spacing}>
-      {repeatingCards}
-    </Grid.Row>
-    <Grid.Row spacing={args.spacing}>
-      <Grid.Column sm={args.sm} md={args.md} lg={args.lg}>
-        <DemoContent label={`Width sm ${args.sm}`} labelSecondary={`Width md ${args.md}`} labelTertiary={`Width lg ${args.lg}`} />
-      </Grid.Column>
-      <Grid.Column>
-        <DemoContent label="Content 2" />
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
-;
-
-export const Default = Template.bind({});
-Default.args = {
+FixedWidthCols.args = {
   fluid: false,
   sm: 0,
   md: 0,
